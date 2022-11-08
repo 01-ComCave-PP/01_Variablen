575ef4190d8fffcb7e2e858b2d1fa35e9af1b3d8
Sebas <cell333666@gmail.com>
2022-11-08 12:07:09 +0100

    Wertzuweisung

--- script_01.js
+++ script_01.js
@@ -10,3 +10,25 @@
 
 // Konsolen-Ausgaben
 // console.log("Hallo");
+// console.log(firstname);
+
+
+// 02 Deklaration + Wertzuweisung
+
+let firstname; // Deklaration (Definition)
+firstname = "otto"; // Wertzuweisung
+console.log(firstname); // Ausgabe
+
+
+let familyname = "Müller";  // Deklaration + Wertzuweisung
+console.log(familyname);  // Ausgabe
+console.log(firstname + " " + familyname); // Ausgabe mit Leerzeichen
+
+
+
+
+
+
+
+
+
